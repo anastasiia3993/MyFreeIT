f9359ddc606a74bfdfedb9a911ab2b3a6d0a1b50
anastasiia baidiuk <anastasiia.baidiuk@visionlake.com>
2022-08-14 11:36:39 +0200

    add logs

--- task_8.py
+++ task_8.py
@@ -3,14 +3,17 @@ from datetime import date, timedelta
 import json
 import logging
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename = "my.log",
-    format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
-    datefmt='%H:%M:%S',
-    )
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-logging.info('Hello')
+url = "https://openexchangerates.org"
+
+logger.info("Sending HTTP GET")
+resp = requests.get(url)
+
+logger.info("Sending HTTP POST")
+resp = requests.post(url, data='My Test Data')
+
 
 
 def write(path, mode, string):
@@ -22,6 +25,8 @@ def main():
     start_date = date(2022, 7, 8)
     end_date = date(2022, 8, 8)
     delta = timedelta(days=1)
+    logging.info('Started')
+
     while start_date <= end_date:
         a = start_date.strftime("%Y-%m-%d")
         start_date += delta
@@ -31,12 +36,13 @@ def main():
         data = res.json()
     with open('history.txt', 'w') as convert_file:
         convert_file.write(json.dumps(data))
-
+    logging.info('Finished')
 
 if __name__ == "__main__":
 	main()
 
 def handbook():
+    logging.info('Started')
     res = requests.get("https://openexchangerates.org/api/currencies.json?app_id=0bf14041e40b4abab54cd5e6132a0d90")
     if res.status_code != 200:
         raise Exception("ERROR: API rquest unsuccessful.")
@@ -44,7 +50,7 @@ def handbook():
 
     with open('currencies.txt', 'w') as convert_file:
         convert_file.write(json.dumps(data2))
-
+    logging.info('Finished')
 handbook()
 
 
